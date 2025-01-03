0f29623336180f902bf86a97983fd65d4b34dc97
Dipak Mali <malidipakb2002@gmail.com>
2025-01-03 14:04:30 +0530

    [MODIFIED]: index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import bookOrder from './controllers/bookOrder';
 import morgan from 'morgan';
 
 const app=express();
-const PORT=5000;
+const PORT=process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
